876e580a9f10ac21c67f7eac556e91033359f3a5
mercyy21 <mawopetu21@gmail.com>
2024-05-10 10:41:11 +0100

    changes

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31fe60490d962fa7d37a39ab5149fc32940d7a49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c697cb6589ba1718d4cb29aa7957ad23853949")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Mini_Core_Banking_Project/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Mini_Core_Banking_Project/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d9ded94daafd885406062e48634dafe7434f8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c697cb6589ba1718d4cb29aa7957ad23853949")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

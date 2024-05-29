4ea9dd482082dad91b057c5577128b3f56fc7e26
mercyy21 <mawopetu21@gmail.com>
2024-05-29 10:44:33 +0100

    test changes

--- src/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ src/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b814a3b02eaad8d53abd08ccecd18e4fe5a80658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4272b026060ffefc290d4a86de2d20171b0952")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
